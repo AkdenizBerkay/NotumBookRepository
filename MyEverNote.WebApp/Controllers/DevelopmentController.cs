e564614d1a123d2f65bcce2b40f3303b4576e1eb
Berkay Akdeniz <akdenizberkay@gmail.com>
2018-09-14 22:11:56 +0300

    MvcGrid Commit

---
+++ MyEverNote.WebApp/Controllers/DevelopmentController.cs
@@ -0,0 +1,22 @@
+﻿using MyEverNote.BusinessLayer;
+using MyEverNote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEverNote.WebApp.Controllers
+{
+    public class DevelopmentController : Controller
+    {
+        // GET: Development
+        public ActionResult MvcGrid()
+        {
+            List<Note> Notes = new List<Note>();
+            Manager<Note> NoteBusiness = new Manager<Note>();
+            Notes = NoteBusiness.GetAll().ToList();
+            return View(Notes);
+        }
+    }
+}
